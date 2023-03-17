4a3c4bbc6058abcffcc080b73ed917c3bedefbcb
lanlin <lanlin0806@icloud.com>
2023-03-17 16:32:26 +0800

    add

---
+++ lib/core/router/routers.dart
@@ -0,0 +1,21 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class LLRouter {
+  static final String initialRoute = "";
+
+  static final Map<String,WidgetBuilder> routers = {
+
+
+  };
+
+  static final RouteFactory generateRoute = (settings) {
+    return null;
+  };
+
+  static final RouteFactory unknownRoute = (settings) {
+    return null;
+  };
+
+}
