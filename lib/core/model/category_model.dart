779c4061cbe680270e0818c20ee8b44e6751d109
lanlin <lanlin0806@icloud.com>
2023-03-21 20:50:29 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	build/4f2a58af27abc49ecf55570043bbf938.cache.dill.track.dill

--- lib/core/model/category_model.dart
+++ lib/core/model/category_model.dart
@@ -1,1 +1,31 @@
-import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+
+class LLCategoryModel {
+  String id;
+  String title;
+  String color;
+  Color cColor;
+
+  LLCategoryModel({
+    required this.id,
+    required this.title,
+    required this.color,
+    required this.cColor
+  });
+
+  factory LLCategoryModel.fromJson(Map<String, dynamic> json) => LLCategoryModel(
+    id : json['id'],
+    title : json['title'],
+    color : json['color'],
+    cColor : Color(int.parse(json['color'], radix: 16) | 0xFF000000),
+  );
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['title'] = title;
+    data['color'] = color;
+    return data;
+  }
+
+}
--- lib/core/model/detail_model.dart
+++ lib/core/model/detail_model.dart
@@ -3,3 +3,41 @@ import 'dart:convert';
 class LLDetailModel {
 
 }
+
+
+class Status {
+  int code;
+  String msg;
+
+  Status({required this.code ,required this.msg});
+
+   factory Status.fromJson(Map<String , dynamic> json) => Status(
+       code: json["code"],
+       msg: json["msg"]
+   );
+
+  Map<String, dynamic> toJson() => {
+    "code": code,
+    "msg": msg,
+  };
+}
+
+
+
+
+
+// 反转map的键与值
+class EnumValues<T>{
+  Map<String,T> map;
+  Map<T,String>reverseMap = {};// 默认为空
+
+  EnumValues(this.map);
+
+  Map<T , String> get reverse{
+    if (reverseMap.isEmpty) {
+      reverseMap = map.map((key, value) => MapEntry(key as T, value as String));
+    }
+    return reverseMap;
+  }
+
+}
