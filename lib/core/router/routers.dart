7e6a48dc1cf30d3020d6da68fa1edc542b23d41d
lanlin <lanlin0806@icloud.com>
2023-03-29 19:53:51 +0800

    add：收藏模块相关逻辑

--- lib/core/router/routers.dart
+++ lib/core/router/routers.dart
@@ -1,8 +1,10 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/core/model/meal_model.dart';
 import 'package:my_flutter_app/core/router/unknown.dart';
 import 'package:my_flutter_app/main.dart';
+import 'package:my_flutter_app/ui/pages/detail/detail.dart';
 import 'package:my_flutter_app/ui/pages/main/main.dart';
 import 'package:my_flutter_app/ui/pages/meal/meal.dart';
 
@@ -12,21 +14,24 @@ class LLRouter {
   static final Map<String,WidgetBuilder> routers = {
      LLMainScreen.routeName: (ctx) => LLMainScreen(),
      LLMealScreen.routeName: (ctx) => LLMealScreen(),
+     // LLDetailScreen.routeName: (ctx) => LLDetailScreen(),
   };
 
-  static final RouteFactory generateRoute = (settings) {
+  // 当routers没有注册相关路由widget 则会执行下面的方法
+  static RouteFactory get generateRoute => (settings) {
     logd("settings.name: $settings.name");
-    if (settings.name == LLMainScreen.routeName){
+    if (settings.name == LLDetailScreen.routeName){
       return  MaterialPageRoute(
           builder: (ctx) {
-            return LLMainScreen();
+            return LLDetailScreen(settings.arguments as LLMealModel);
           }
       );
     }
     return null;
   };
 
-  static final RouteFactory unknownRoute = (settings) {
+  // 当routers没有注册相关路由widget 而且generateRoute方法返回null 则会执行此方法返回异常页面
+  static RouteFactory get unknownRoute => (settings) {
     return MaterialPageRoute(
         builder: (ctx) {
           return LLUnknownPage();
--- lib/ui/pages/detail/detail.dart
+++ lib/ui/pages/detail/detail.dart
@@ -3,18 +3,22 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_flutter_app/core/model/meal_model.dart';
 
+
 class LLDetailScreen extends StatelessWidget {
   static const String routeName = "/detail";
+  final LLMealModel mealModel;
+
+  LLDetailScreen(this.mealModel);
 
   @override
   Widget build(BuildContext context) {
-    final model = ModalRoute.of(context)?.settings.arguments as LLMealModel;
+    // final model = ModalRoute.of(context)?.settings.arguments as LLMealModel;
     return Scaffold(
       appBar: AppBar(
-        title: Text(model.title),
+        title: Text(mealModel.title),
       ),
       body: Center(
-        child: Text(model.title),
+        child: Text(mealModel.title),
       ),
     );
   }
